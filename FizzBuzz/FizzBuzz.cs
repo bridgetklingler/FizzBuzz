64b96111a31c31d7486fbf2ccdad44d017165f97
Bridget Klingler <bridget.klingler@gmail.com>
2019-05-29 23:59:46 -0400

    cleaner margins in FizzBuzz class

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -12,24 +12,28 @@ namespace FizzBuzz
             {
                 return "FizzBuzz";
             }
+
+            if (IsMultipleOf3(num))
             {
-                if (IsMultipleOf3(num))
                 return "Fizz";
             }
+
             if (IsMultipleOf5(num))
             {
                 return "Buzz";
             }
             return num.ToString();
         }
+
         private bool IsMultipleOf3(int numberToCheck)
-        {
-            return numberToCheck % 3 == 0;
-        }
+            {
+                return numberToCheck % 3 == 0;
+            }
+
         private bool IsMultipleOf5(int numberToCheck)
-        {
-            return numberToCheck % 5 == 0;
-        }
+            {
+                return numberToCheck % 5 == 0;
+            }
                 /*if (num % 3 == 0)
                 {
                     return "Fizz";
